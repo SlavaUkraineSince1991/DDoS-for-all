716177ea1edab04eda661a3dac1e397a994a17e6
SlavaUkraineSince1991 <101798905+SlavaUkraineSince1991@users.noreply.github.com>
2022-03-18 15:33:23 +0200

    Create docker_install.sh

---
+++ scripts/docker_install.sh
@@ -0,0 +1,14 @@
+# Script for installing Docker for Ubuntu
+# To run it just paste the following command to your Terminal:
+# curl -s https://raw.githubusercontent.com/SlavaUkraineSince1991/DDoS-for-all/scripts/main/docker_install.sh | bash
+
+sudo apt-get update
+sudo apt-get install ca-certificates gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
