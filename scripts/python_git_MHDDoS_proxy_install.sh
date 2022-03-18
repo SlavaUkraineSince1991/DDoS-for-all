9afab7f3b316a1d5a4608e0ec6c7b0f9343ed322
SlavaUkraineSince1991 <101798905+SlavaUkraineSince1991@users.noreply.github.com>
2022-03-18 16:26:59 +0200

    Create python_git_MHDDoS_proxy_install.sh

---
+++ scripts/python_git_MHDDoS_proxy_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##### Script for installing Python, Git and MHDDoS_proxy for Ubuntu
+##### To run it just paste the following command to your Terminal:
+# >>>>> curl -s https://raw.githubusercontent.com/SlavaUkraineSince1991/DDoS-for-all/main/scripts/python_git_MHDDoS_proxy_install.sh | bash <<<<<
+
+# Install git, python3, pip if doesn't installed already
+sudo sudo apt update -y
+if [ ! -f /usr/bin/git ]; then
+   sudo apt install git -y
+fi
+if [ ! -f /usr/bin/python3 ]; then
+   sudo apt install python3 -y
+fi
+if [ ! -f /usr/bin/pip ]; then
+   apt install python3-pip  -y
+fi
+if [ ! -f /usr/bin/wget ]; then
+   apt install wget  -y
+fi
+if [ ! -f /usr/bin/curl ]; then
+   apt install curl  -y
+fi
+pip install --upgrade pip > /dev/null #No output. Resolved some problems with pip on debian
+
+#Install latest version of mhddos_proxy and MHDDoS
+cd ~
+rm -rf mhddos_proxy
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+git clone https://github.com/MHProDev/MHDDoS.git
+python3 -m pip install -r MHDDoS/requirements.txt
