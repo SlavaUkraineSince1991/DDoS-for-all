cc1924f2e2a412baf0da509bfe7e9482255befcb
SlavaUkraineSince1991 <101798905+SlavaUkraineSince1991@users.noreply.github.com>
2022-03-25 14:22:44 +0200

    Update python_git_MHDDoS_proxy_install.sh

--- scripts/python_git_MHDDoS_proxy_install.sh
+++ scripts/python_git_MHDDoS_proxy_install.sh
@@ -28,5 +28,4 @@ cd ~
 rm -rf mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
-git clone https://github.com/porthole-ascend-cinnamon/MHDDoS.git
-python3 -m pip install -r MHDDoS/requirements.txt
+python3 -m pip install -r requirements.txt
